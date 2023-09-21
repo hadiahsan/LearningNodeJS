f42205540f45dbb66e5956b2068caafcb8ff7079
Hadi Ahsan <hadi.ahsan@hotmail.com>
2023-09-21 15:48:02 -0400

    Adds the 'path' module in Node

--- server.js
+++ server.js
@@ -6,7 +6,15 @@
 // 5) CommonJS modules instead of ES6 modules
 
 const os = require('os')
+const path = require('path')
 
 console.log(os.type())
 console.log(os.version())
 console.log(os.homedir())
+
+console.log(__dirname)
+console.log(__filename)
+
+console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
